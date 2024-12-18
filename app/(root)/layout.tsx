5d5bbb09a636b794ff05f7d65dcd684024d9798a
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-12-18 05:32:06 +0530

    Edit changes

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+import Navbar from "@/components/Navbar";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Job Search - Flint",
+  description: "Find your dream job with Flint",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+}

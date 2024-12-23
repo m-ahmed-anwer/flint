2eea80901cfba92d83b5fe1b42187aba7c3e67af
m-asjedh <m.asjedhcr7@gmail.com>
2024-12-23 19:54:19 +0530

    feat: flint-3 Login form created

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,21 +1,45 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { Button } from "./ui/button";
 
 const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div className="mt-6">
-      <div className="space-y-4">
-        <input
-          type="email"
-          placeholder="Enter Email"
-          className="border rounded-lg p-2 w-full"
-        ></input>
-        <input
-          type="password"
-          placeholder="Enter Passowrd"
-          className="border rounded-lg p-2 w-full"
-        ></input>
+    <form>
+      <div className="mt-6">
+        <div className="space-y-4">
+          <input
+            type="email"
+            placeholder="Enter Email"
+            className="border rounded-lg p-2 w-full"
+          />
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Enter Password"
+              className="border rounded-lg p-2 w-full"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              type="button"
+              onClick={togglePassword}
+              className="absolute right-2 top-2"
+            >
+              {showPassword ? "🙈" : "👁️"}
+            </button>
+          </div>
+          <Button className="w-full">Login</Button>
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
 
